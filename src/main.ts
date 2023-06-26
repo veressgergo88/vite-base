381b4b23db850a04480155214473d9c9d66cdf47
veressgergo88 <veress.gergo@gmail.com>
2023-06-26 21:11:54 +0200

    added load async, Zod schema, Load button, connect the api

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,35 @@
 import './style.css'
+import http from 'axios'
+import { z } from 'zod'
 
-console.log("Hello World")
+const CharacterResponseSchema = z.object({
+  info: z.object({
+    next: z.string()
+  }),
+  results: z.object({
+    name: z.string()
+  }).array()
+})
+
+type CharacterResponse = z.infer<typeof CharacterResponseSchema>
+
+const load = async (): Promise<CharacterResponse | null> => {
+  const response = await http.get("https://rickandmortyapi.com/api/character")
+  const data = response.data
+
+  const result = CharacterResponseSchema.safeParse(data)
+
+  if(!result.success){ 
+    console.log(result.error)
+    return null
+  }
+  return result.data
+}
+
+const init = async () => {
+  const characters = await load()
+  if (characters)
+    document.getElementById("app")!.innerHTML = characters!.results[0].name
+}
+
+document.getElementById("load-button")!.addEventListener("click", init)
